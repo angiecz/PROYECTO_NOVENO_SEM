f443060df4a98916ba5446b058c92eabc63b5e3a
angiecz <89753001+angiecz@users.noreply.github.com>
2022-05-18 14:06:40 -0500

    MIsion y vision

--- Vista/Usuario/planeacion.php
+++ Vista/Usuario/planeacion.php
@@ -2,19 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="forte.js"></script>
 	<link rel="stylesheet" href="../CSS/Planeacion.css">
-  
         <title>Planeación estratégica</title>
     </head>
     <body>
-        <section id="gracias">
-        </section>
+      
     </body>
+    </html>
